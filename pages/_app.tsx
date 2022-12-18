a68c3bbe4dfac96ee0df06d240069896d70e893e
Josh <josh.k@tad.sr>
2022-12-17 21:38:14 -0300

    feat: budget context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { WishlistProvider } from "react-use-wishlist";
+import { BudgetProvider } from "../context/budgetContext";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,9 +12,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>BudgetBear</title>
         <link rel="shortcut icon" href="/BudgetBear.png" />
       </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-      </MantineProvider>
+      <BudgetProvider>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <WishlistProvider>
+            <Component {...pageProps} />
+          </WishlistProvider>
+        </MantineProvider>
+      </BudgetProvider>
     </>
   );
 }
